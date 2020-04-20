37804216c1ec85c0357f02aa6d8d49306c919e66
Shakore McGill <mcgills2093@gmail.com>
2020-04-20 20:34:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,11 +25,10 @@ end
 
 def return_string_array(array)
   counter = 0 
+  string_array =[ ]
   while counter < array.length do 
-    array[counter].to_s
+    string_array.push(array[counter].to_s)
     counter += 1 
   end
-end
-  
-  
+  string_array
 end
