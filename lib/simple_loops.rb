4fccabbe8922ebfb9afd3f29a029b7959e557605
Shakore McGill <mcgills2093@gmail.com>
2020-04-20 20:29:07 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,30 @@
 # Write your methods here
+def loop_message_five_times(message)
+  i = 0
+  while i < 6
+    puts message
+    i += 1
+  end
+end
+
+def loop_message_n_times(message, number)
+  number = 0 
+   while message[number] do
+     puts message
+     number += 1 
+   end
+end
+
+def output_array(array)
+  counter = 0 
+  while array[counter]do
+    puts array[counter]
+    counter += 1
+  end
+end
+
+def return_string_array(array)
+  
+  
+  
+end
